00ac755c9f81bf0bc7defb15e4bbcf173c39ad83
Andre Austin <austinaj2@vcu.edu>
2022-02-25 22:01:22 -0500

    Initial commit

---
+++ Eulerity API Images/ImageCell.swift
@@ -0,0 +1,26 @@
+//
+//  ImageCell.swift
+//  Eulerity API Images
+//
+//  Created by Macky Cisse on 2/25/22.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var cellLabelView: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Eulerity API Images/ViewController.swift
+++ Eulerity API Images/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var imagesTable: UITableView!
+    
+    let images: [Response] = [
+        Response(url: "URL", created: "Now", updated: "Again"),
+        Response(url: "URL2", created: "Now", updated: "Again"),
+        Response(url: "URL3", created: "Now", updated: "Again")
+        
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagesTable.delegate = self
+        imagesTable.dataSource = self
+
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = imagesTable.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as? ImageCell {
+            let r = images[indexPath.row]
+            cell.cellImageView.image = UIImage(named: "cart")
+            cell.cellLabelView.text = r.url
+            print("\(r.url)")
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    
 
 
+}
+    
+struct Response: Codable {
+    let url: String
+    let created: String
+    let updated: String
 }
 
